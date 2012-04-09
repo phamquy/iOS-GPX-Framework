393d18584a88ac620fd493559fbb7e1509ee76bd
Watanabe Toshinori <t@flcl.jp>
2012-04-09 16:54:38 +0900

    initial import for GitHub

---
+++ GPX/GPX.h
@@ -0,0 +1,28 @@
+//
+//  GPX.h
+//  GPX Framework
+//
+//  Created by NextBusinessSystem on 12/04/06.
+//  Copyright (c) 2012 NextBusinessSystem Co., Ltd. All rights reserved.
+//
+
+#import <GPX/GPXParser.h>
+#import <GPX/GPXConst.h>
+#import <GPX/GPXType.h>
+#import <GPX/GPXRoot.h>
+#import <GPX/GPXMetadata.h>
+#import <GPX/GPXWaypoint.h>
+#import <GPX/GPXRoutePoint.h>
+#import <GPX/GPXTrackPoint.h>
+#import <GPX/GPXRoute.h>
+#import <GPX/GPXTrack.h>
+#import <GPX/GPXExtensions.h>
+#import <GPX/GPXTrackSegment.h>
+#import <GPX/GPXCopyright.h>
+#import <GPX/GPXLink.h>
+#import <GPX/GPXEmail.h>
+#import <GPX/GPXPerson.h>
+#import <GPX/GPXAuthor.h>
+#import <GPX/GPXPoint.h>
+#import <GPX/GPXPointSegment.h>
+#import <GPX/GPXBounds.h>
---
+++ GPXTests/GPXTests.h
@@ -0,0 +1,13 @@
+//
+//  GPXTests.h
+//  GPXTests
+//
+//  Created by 俊紀 渡辺 on 12/04/06.
+//  Copyright (c) 2012年 _MyCompanyName_. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GPXTests : SenTestCase
+
+@end
